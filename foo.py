b7366ea17d7164d23473f1f893e0eb515e7a4cbd
hnc-yusop <83211009+hnc-yusop@users.noreply.github.com>
2022-03-10 14:40:47 +0900

    Update foo.py

--- foo.py
+++ foo.py
@@ -5,7 +5,5 @@ app = Flask(__name__)
 def hello():
     return '[ {"user": "geert", "group": "admins"}, {"user": "bobby", "group": "users"} ]'
 
-
-
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
